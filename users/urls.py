a34b06d4387a70f4fad3de12102df1cc43080a70
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2025-04-07 22:01:19 +0600

    UI Update

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
-from .views import CustomPasswordResetView,SignupView
+from .views import CustomPasswordResetView, SignupView, CustomPasswordResetConfirmView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('', include('django.contrib.auth.urls')),
+    path("password_reset/", CustomPasswordResetView.as_view(html_email_template_name="registration/password_reset_html_email.html"), name="password_reset"),
+    path(
+        "reset/<uidb64>/<token>/",
+        CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("", include("django.contrib.auth.urls")),
 ]
-app_name='users'
+app_name = "users"
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
+from typing import Any
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django import forms
@@ -10,31 +14,33 @@ from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 
 User = get_user_model()
-# Create your views here.
-class CustomPasswordResetForm(forms.Form):
-    email = forms.EmailField(
-        label="Email",
-        max_length=254,
-        widget=forms.EmailInput(attrs={"autocomplete": "email"})
-    )
 
+
+# Create your views here.
+class CustomPasswordResetForm(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data["email"]
         if not get_user_model().objects.filter(email=email).exists():
-            raise ValidationError("There is no user registered with this email address.")
+            raise ValidationError(
+                "There is no user registered with this email address."
+            )
         return email
+
 
 class CustomPasswordResetView(PasswordResetView):
     form_class = CustomPasswordResetForm
+    success_url = reverse_lazy("users:password_reset_done")
 
 
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    success_url = reverse_lazy("users:password_reset_complete")
 
 
 class SignupView(CreateView):
-    model=User
+    model = User
     form_class = CustomUserCreationForm
     template_name = "registration/signup.html"
-    success_url = reverse_lazy('users:login')
-    
+    success_url = reverse_lazy("users:login")
+
     def get_success_url(self):
         return settings.SIGNUP_REDIRECT_URL
